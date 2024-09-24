a70a9b63bdc3bea98766a21e2c2523d33eb80b33
dr250385 <dr250385@WIN-10M20X2>
2024-09-24 10:44:24 +0200

    handling file input

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -8,9 +8,41 @@ export default function Signup() {
   const [password, setPassword] = useState('');
   const [displayName, setDisplayName] = useState('');
   const [thumbnail, setThumbail] = useState(null);
+  const [thumbnailError, setThumbnailError] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(email, password, displayName, thumbnail);
+  };
+
+  const handleFileChange = (e) => {
+    setThumbail(null);
+
+    let selected = e.target.files[0];
+
+    if (!selected) {
+      setThumbnailError('Please select a file');
+      return;
+    }
+
+    if (!selected.type.includes('image')) {
+      setThumbnailError('Selected file must be an image');
+      return;
+    }
+
+    if (selected.size > 100000) {
+      setThumbnailError('Image file size must be less than 100kb');
+      return;
+    }
+
+    setThumbnailError(null);
+    setThumbail(selected);
+    console.log('thumbnail updated');
+  };
 
   return (
-    <form className='auth-form'>
+    <form className='auth-form' onSubmit={handleSubmit}>
       <h2>Sign up</h2>
       <label>
         <span>email:</span>
@@ -33,7 +65,7 @@ export default function Signup() {
       </label>
 
       <label>
-        <span>text:</span>
+        <span>display name:</span>
         <input
           type='text'
           required
@@ -44,7 +76,9 @@ export default function Signup() {
 
       <label>
         <span>profile thumbnail:</span>
-        <input type='file' required />
+        <input type='file' required onChange={handleFileChange} />
+
+        {thumbnailError && <div className='error'>{thumbnailError}</div>}
       </label>
 
       <button className='btn'>Sign Up</button>
