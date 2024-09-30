2e9fa002aeb59a678f7f42d49578d2bfcd6c983a
dr250385 <dr250385@WIN-10M20X2>
2024-09-30 13:56:19 +0200

    making useDocument hook

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -1,13 +1,31 @@
 // styles
+import Avatar from './Avatar';
 import './ProjectList.scss';
+
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 export default function ProjectList({ projects }) {
   return (
-    <div>
+    <div className='project-list'>
       {projects.length < 1 ? (
         <p>No projects yet</p>
       ) : (
-        projects.map(({ name, id }) => <div key={id}>{name}</div>)
+        projects.map(({ name, id, dueDate, assignedUsersList }) => (
+          <Link to={`/projects/${id}`} key={id}>
+            <h4>{name}</h4>
+            <p>Due by {dueDate.toDate().toDateString()}</p>
+
+            <div className='assigned-to'>
+              <ul>
+                {assignedUsersList.map(({ photoURL }) => (
+                  <li key={photoURL}>
+                    <Avatar src={photoURL}></Avatar>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </Link>
+        ))
       )}
     </div>
   );
