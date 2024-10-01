7d8790b673d32477f64976cb9a10f42a21dd463d
dr250385 <dr250385@WIN-10M20X2>
2024-10-01 12:44:44 +0200

    completing/deleting project

--- src/pages/project/ProjectComments.jsx
+++ src/pages/project/ProjectComments.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { timestamp } from '../../firebase/config';
 import { useAuthContext } from '../../hooks/useAuthContext';
 import { useFirestore } from '../../hooks/useFirestore';
+import Avatar from '../../components/Avatar';
 
 export default function ProjectComments({ project }) {
   const [newComment, setNewComment] = useState('');
@@ -30,8 +31,28 @@ export default function ProjectComments({ project }) {
   };
 
   return (
-    <div className='project-projects'>
+    <div className='project-comments'>
       <h4>Project Comments</h4>
+
+      <ul>
+        {project.comments.length > 0 &&
+          project.comments.map((comment) => (
+            <li key={comment.id}>
+              <div className='comment-author'>
+                <Avatar src={comment.photoURL}></Avatar>
+                <p>{comment.displayName}</p>
+              </div>
+
+              <div className='comment-date'>
+                <p>date here</p>
+              </div>
+
+              <div className='comment-content'>
+                <p>{comment.content}</p>
+              </div>
+            </li>
+          ))}
+      </ul>
 
       <form className='add-coment' onSubmit={handleSubmit}>
         <label>
